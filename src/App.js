a720705a008b0655875db79513cc8ba98c18a835
Jonathan Jorge <jcanola canolaj>
2021-11-16 22:12:24 -0500

    Add React-Bootstrap, folder components->Nav.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
@@ -7,8 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> and save to reload.
         </p>
+        
+        <Nav />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+
+const nav = () => {
+    return (
+        <>
+            <h1>hola desde el nav</h1>
+            <p>hi</p>
+        </>
+    );
+}
+
+export default nav;
